764542cde59c83a799df5a6f6e23e2701f75dcef
Jennifer A Causing <jac84@afsconnect1.njit.edu>
2016-09-29 13:36:39 -0400

    worked in wrong file woops. added text

---
+++ helloworld.php
@@ -0,0 +1,11 @@
+<?
+
+	echo 'Hello World!';
+	$myvar = 'text';
+	echo '<br>';
+	//single quote = exact var
+	echo '$myvar';
+	echo '<br>';
+	//double quote = everything is printed as a quote
+	echo "$myvar";
+?>
