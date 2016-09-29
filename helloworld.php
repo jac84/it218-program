d5ca8bfb28a6b621a84de1c66a1a73bee861fe23
Jennifer A Causing <jac84@afsconnect1.njit.edu>
2016-09-29 13:50:26 -0400

    worked in correct file. added more examples

--- helloworld.php
+++ helloworld.php
@@ -1,11 +1,27 @@
-<?
+<?php
 
 	echo 'Hello World!';
 	$myvar = 'text';
 	echo '<br>';
-	//single quote = exact var
-	echo '$myvar';
+
+	echo '$myvar'; //prints $myvar
 	echo '<br>';
-	//double quote = everything is printed as a quote
-	echo "$myvar";
+	echo "$myvar"; //prints 'text'
+
+	echo '<br>';
+
+	//example of php array
+	$myarray = array(); //create empty array
+	$myarray[] = 'some value 1'; //push a value into array
+	$myarray[] = 'some value 2';
+	$myarray[] = 'some value 3';
+
+	print_r($myarray); //IMPORTANT: View inside an object or array
+	//print works the same as echo, print_r is different
+
+	echo '<br>';
+
+	$myAssoc = array('value1' => $myarray, 'value2' => $myarray); //associative array has named key
+	//associative arrays can embed array within itself
+	print_r($myAssoc);
 ?>
