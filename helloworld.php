426c0cac90267b7655fbb2b8e9b4da8dffbf8f11
Jennifer A Causing <jac84@afsconnect1.njit.edu>
2016-09-29 14:24:42 -0400

    added more examples

--- helloworld.php
+++ helloworld.php
@@ -1,7 +1,7 @@
 <?php
 
 	echo 'Hello World!';
-	$myvar = 'text';
+	$myvar = 'hey some text';
 	echo '<br>';
 
 	echo '$myvar'; //prints $myvar
@@ -24,4 +24,38 @@
 	$myAssoc = array('value1' => $myarray, 'value2' => $myarray); //associative array has named key
 	//associative arrays can embed array within itself
 	print_r($myAssoc);
+
+	echo '<br>';
+
+	print_r($myAssoc['value1']); //prints value in key 'value1'
+
+	echo '<br>';
+
+	class myclass { //class = schematic/blueprint for an object
+		public $myPublic;
+		private $myPrivate;
+		protected $myProtected;
+
+		//review SCOPE OF VARIABLES
+		//vars in class = properties. vars outside class = vars
+		public function __construct(){ //~*magic method*~ automatically does stuff
+		/*"$this" is an internal reference to the object within a class. -> points to a property or method being accessed within the object*/
+			$this->myPublic = 1; //can be accessed externally outside of class
+			$this->myPrivate = 2; //only accessed inside class; hidden
+			$this->myProtected = 3; /*inbetween public & private. can be accessed within classes and
+			subclasses but cannot be accessed externally when obj is instantiated
+			think like a jail cell*/
+
+			$this->sayHello('Jay'); //call a method within an object & pass a var
+		}
+
+	echo '<br>';
+
+		public function sayHello($name){
+			echo 'Hello ', $name;
+		}
+	}
+
+	$obj = new myclass; //instantiate a new class object
+	print_r($obj); //print_r also shows objects
 ?>
